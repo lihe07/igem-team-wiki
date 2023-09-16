76568c5d00a9ec253084fbe1a7ab6917534c963d
lihe07 <li@imlihe.com>
2023-09-16 21:46:46 +0800

    video performance

--- src/routes/Video.svelte
+++ src/routes/Video.svelte
@@ -5,21 +5,58 @@
   // import ScrollyVideo from "scrolly-video/dist/ScrollyVideo.svelte";
   export let percent = 0;
 
-  let bloburl = "";
-  onMount(async () => {
-    // frames = await extractFramesFromVideo(
-    //   "https://scrollyvideo.js.org/goldengate.mp4"
-    // );
+  const video =
+    "https://lms.d.zhan.com/zhanlms/addon_homework/2023/09/30964656505b16990208/goldengate.mp4";
 
-    const resp = await fetch("https://scrollyvideo.js.org/goldengate.mp4");
+  const rvideo =
+    "https://lms.d.zhan.com/zhanlms/addon_homework/2023/09/40871056505b194584eb/rgoldengate.mp4";
 
-    const blob = await resp.blob();
-    bloburl = URL.createObjectURL(blob);
+  let reverse = false;
+
+  let last_p = 0;
+
+  $: p = percent / 10;
+
+  function check() {
+    if (p - last_p > 0) {
+      reverse = false;
+    }
+
+    if (p - last_p < 0) {
+      reverse = true;
+    }
+
+    last_p = p;
+
+    setTimeout(check, 100);
+  }
+
+  onMount(() => {
+    check();
   });
 </script>
 
-<div style:opacity={percent + 1} style="">
-  {#if bloburl.length}
-    <ScrollyVideo src={bloburl} videoPercentage={percent / 10} frameRate={60} />
-  {/if}
+<div style:opacity={percent + 1} style="" class="c">
+  <div style="opacity: {reverse ? 1 : 0};" class="v">
+    <ScrollyVideo src={rvideo} videoPercentage={1 - p} frameRate={60} />
+  </div>
+  <div style="opacity: {reverse ? 0 : 1};" class="v">
+    <ScrollyVideo src={video} videoPercentage={p} useWebCodec={false} />
+  </div>
 </div>
+
+<style>
+  .c {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+
+  .v {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    left: 0;
+  }
+</style>
